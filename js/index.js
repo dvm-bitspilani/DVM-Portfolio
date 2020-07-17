ff28b4d0514231f55cebcf317d38d03204073449
iagrg2001 <igarg2001@gmail.com>
2020-07-17 20:15:10 +0530

    fixed mobile scroll(now working), added arrow up/down scroll

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-//console.log(document.documentElement.getBoundingClientRect().top)
-//var FontFaceObserver = require('fontfaceobserver');
-
 var font = new FontFaceObserver('Jaapokki subtract')
 font.load().then(() => {
     //This is only a demo for preloading fonts. Use ONLY for loader fonts strictly
@@ -15,16 +12,14 @@ font.load().then(() => {
 let sectionNo = 0
 let scrollDebounce = true
 document.documentElement.scrollTo(0,0)
-//window.scrollTo(0,0)
-//console.log(document.getElementsByClassName('page')[++sectionNo].getBoundingClientRect().top)
 const fromTop = document.getElementsByClassName('page')[1].getBoundingClientRect().top
-console.log(fromTop)
 const pages = document.getElementsByClassName('page')
-//console.log(window.innerHeight)
 
+
+
+//generic function for scrolling the section
 const scrollSection = distance => {
     if(scrollDebounce) {
-        //console.log(scrollDebounce)
         scrollDebounce=false
         if(distance>0 || distance==='down') {
             if(pages[pages.length-1].getBoundingClientRect().top<=0) {
@@ -32,11 +27,6 @@ const scrollSection = distance => {
                 return
             }
             let scrollAmt = fromTop
-            // if(sectionNo>=document.getElementsByClassName("page").length-1) {
-            //     console.log("returning")
-            //     return
-            // }
-            //console.log(top- 0.5*distance)
             window.scrollBy({
                 top: scrollAmt,
                 left:0,
@@ -44,9 +34,6 @@ const scrollSection = distance => {
             })
          }
          if(distance<0 || distance==='up') {
-            //  if(sectionNo<=0)
-            //     return
-             //console.log("hi")
              window.scrollBy({
                 
                 top: -fromTop,
@@ -56,10 +43,21 @@ const scrollSection = distance => {
          }
         setTimeout(() => scrollDebounce=true, 500)
     }
-    //console.log(distance)
 }
 
+
+//Below line for touchpad and mousewheel swipe
 window.addEventListener("wheel", e=> scrollSection(e.deltaY))
+
+window.addEventListener('keyup', e=> {
+    if(e.keyCode===40) {
+        scrollSection('down')
+    }
+    if(e.keyCode===38) {
+        scrollSection('up')
+    }
+
+})
 
 let startX, startY, distX, distY, swipedir, 
 threshold=150,
@@ -68,6 +66,8 @@ allowdTime=300,
 elaspedTime,
 startTime;
 
+
+//Code for registering touchswipe
 window.addEventListener('touchstart', e => {
     var touchObj = e.changedTouches[0]
     swipedir = 'none'
@@ -92,7 +92,7 @@ window.addEventListener('touchend', e=> {
             swipedir= distX<0 ? 'left': 'right'
         }
         else if(Math.abs(distY)>=threshold && Math.abs(distX)<=restraint) {
-            swipedir= distY<0 ? 'up': 'down'
+            swipedir= distY<0 ? 'down': 'up'
         }
     }
     e.preventDefault()
