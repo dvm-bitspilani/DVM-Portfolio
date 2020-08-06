3eea3919affb6e4bfe84266f91dc0c2ef7603d2b
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2020-08-06 22:53:09 +0530

    landing page updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 var font = new FontFaceObserver('Jaapokki subtract')
 font.load().then(() => {
     //This is only a demo for preloading fonts. Use ONLY for loader fonts strictly
@@ -209,3 +211,81 @@ buttonRightBlog.onclick = function () {
 buttonLeftBlog.onclick = function () {
   document.getElementById('scrolling-wrapper').scrollLeft -= 100;
 };
+
+(function() {
+    // Init
+    var container = document.getElementById("homeLogoContainer"),
+        inner = document.getElementById("homeLogoInner");
+
+    // Mouse
+    var mouse = {
+        _x: 0,
+        _y: 0,
+        x: 0,
+        y: 0,
+        updatePosition: function(event) {
+            var e = event || window.event;
+            this.x = e.clientX - this._x;
+            this.y = (e.clientY - this._y) * -1;
+        },
+        setOrigin: function(e) {
+            this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+            this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+        },
+        show: function() {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+    };
+
+    // Track the mouse position relative to the center of the container.
+    mouse.setOrigin(container);
+
+    //-----------------------------------------
+
+    var counter = 0;
+    var updateRate = 10;
+    var isTimeToUpdate = function() {
+        return counter++ % updateRate === 0;
+    };
+
+    //-----------------------------------------
+
+    var onMouseEnterHandler = function(event) {
+        update(event);
+    };
+
+    var onMouseLeaveHandler = function() {
+        inner.style = "";
+    };
+
+    var onMouseMoveHandler = function(event) {
+        if (isTimeToUpdate()) {
+            update(event);
+        }
+    };
+
+    //-----------------------------------------
+
+    var update = function(event) {
+        mouse.updatePosition(event);
+        updateTransformStyle(
+            2 * (mouse.y / inner.offsetHeight).toFixed(2),
+            2 * (mouse.x / inner.offsetWidth ).toFixed(2)
+        );
+    };
+
+    var updateTransformStyle = function(x, y) {
+        var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+        inner.style.transform = style;
+        inner.style.webkitTransform = style;
+        inner.style.mozTransform = style;
+        inner.style.msTransform = style;
+        inner.style.oTransform = style;
+    };
+
+    //-----------------------------------------
+
+    container.onmouseenter = onMouseEnterHandler;
+    container.onmouseleave = onMouseLeaveHandler;
+    container.onmousemove = onMouseMoveHandler;
+})();
