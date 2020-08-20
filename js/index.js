4d25189e3b355f08410f0331606f5f163ccaeab4
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2020-08-20 22:13:50 +0530

    Landing back updated basic

--- js/index.js
+++ js/index.js
@@ -307,7 +307,38 @@ const navigateCarousel = (step, stepType) => {
   //-----------------------------------------
 
   container.onmouseenter = onMouseEnterHandler;
-  console.log(container.onmouseenter)
   container.onmouseleave = onMouseLeaveHandler;
   container.onmousemove = onMouseMoveHandler;
 })();
+
+//-----------------
+window.addEventListener("DOMContentLoaded", () => {
+
+  const spotlight = document.querySelector('.spotlight');
+
+  let spotlightSize = 'transparent 49px, rgba(0, 0, 0, 0.85) 92px)';
+
+  window.addEventListener('mousemove', e => updateSpotlight(e));
+
+  window.addEventListener('mousedown', e => {
+
+    spotlightSize = 'transparent 130px, rgba(0, 0, 0, 0.95) 150px)';
+
+    updateSpotlight(e);
+
+  });
+
+  window.addEventListener('mouseup', e => {
+
+    spotlightSize = 'transparent 49px, rgba(0, 0, 0, 0.85) 92px)';
+
+    updateSpotlight(e);
+
+  });
+
+  function updateSpotlight(e) {
+
+    spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+
+  }
+});
