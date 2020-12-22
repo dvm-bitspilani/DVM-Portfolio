d90c395e5dd0f69ddc8d8fa8e7521f9264ea875c
Priyanshu <priyanshushukla9801@gmail.com>
2020-12-22 18:56:53 +0530

    New Scrollbar

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,5 @@
+// Array containing the content
+
 main_arr = [
     [
         {
@@ -92,26 +94,39 @@ main_arr = [
 
 ]
 
-document.getElementsByClassName('projects')[0].scrollRight = 150
+// Constant Variables
 
-team(2)
+const slider = document.querySelector('.projects');
+const inner = document.querySelector('.inner')
+const ele = document.getElementById('slider');
+const left_limit = document.getElementById('dotted_line_wrapper').offsetLeft
+const right_limit = document.getElementById('dotted_line_wrapper').offsetLeft + document.getElementById('dotted_line_wrapper').offsetWidth - document.getElementById('slider').offsetWidth
+const slide_bar_scroll_dist = document.getElementById('dotted_line_wrapper').offsetWidth - document.getElementById('slider').offsetWidth
+
+
+let scroll_dist
+
 
 function team(input) {
     for (var i = 0; i < 5; i++) {
         document.getElementsByClassName('team-name')[i].className = 'team-name'
     }
     document.getElementsByClassName('team-name')[input].className = 'team-name highlighted'
+
+
     team_projects(input)
+
+
 }
 
 function team_projects(input) {
     var window_width = window.innerWidth
-    console.log(window_width)
+
     document.getElementsByClassName('projects')[0].scrollRight = 150
     document.getElementsByClassName('projects')[0].innerHTML = ''
     var no_of_projects = main_arr[input].length
     var total_width = (64 * no_of_projects)
-    console.log(total_width)
+
     document.getElementsByClassName('projects')[0].style.width = total_width + 'vw'
     for (var i = 0; i < no_of_projects; i++) {
         var div = document.createElement('div')
@@ -125,12 +140,18 @@ function team_projects(input) {
         name_div.innerHTML = main_arr[input][i].name
         document.getElementsByClassName('single_project')[i].appendChild(name_div)
     }
+    setTimeout(function () {
+        scroll_dist = document.querySelector('.projects').offsetWidth - window.innerWidth
+        console.log(scroll_dist)
+        inner.scrollLeft = 0
+        ele.style.left = `${left_limit}px`
+    }, 500)
+
 }
 
 // Drag Scroll
 
-const slider = document.querySelector('.projects');
-const inner = document.querySelector('.inner')
+
 let isDown = false;
 let startX;
 let scrollLeft;
@@ -138,16 +159,19 @@ let scrollLeft;
 slider.addEventListener('mousedown', (e) => {
     isDown = true;
     slider.classList.add('active');
+    ele.classList.add('active')
     startX = e.pageX - inner.offsetLeft;
     scrollLeft = inner.scrollLeft;
 });
 slider.addEventListener('mouseleave', () => {
     isDown = false;
     slider.classList.remove('active');
+    ele.classList.remove('active')
 });
 slider.addEventListener('mouseup', () => {
     isDown = false;
     slider.classList.remove('active');
+    ele.classList.remove('active')
 });
 slider.addEventListener('mousemove', (e) => {
     if (!isDown) return;
@@ -155,35 +179,50 @@ slider.addEventListener('mousemove', (e) => {
     const x = e.pageX - inner.offsetLeft;
     const walk = (x - startX) * 2; //scroll-fast
     inner.scrollLeft = scrollLeft - walk;
-    console.log(walk);
+    var slider_offset = (inner.scrollLeft / scroll_dist) * slide_bar_scroll_dist
+
+
+    if (slider_offset > left_limit && slider_offset < right_limit) {
+        ele.style.left = `${slider_offset}px`
+    }
+    else if (slider_offset < left_limit) {
+        ele.style.left = `${left_limit}px`
+    }
+    else {
+        ele.style.left = `${right_limit}px`
+    }
+
 });
 
 // Slider 
 
 let x = 0;
 
-const ele = document.getElementById('slider');
-const left_limit = document.getElementById('dotted_line_wrapper').offsetLeft
-const right_limit = document.getElementById('dotted_line_wrapper').offsetLeft + document.getElementById('dotted_line_wrapper').offsetWidth - document.getElementById('slider').offsetWidth
 const mouseDownHandler = function (e) {
     x = e.clientX;
     ele.classList.add('active')
+    slider.classList.add('active');
     document.addEventListener('mousemove', mouseMoveHandler);
     document.addEventListener('mouseup', mouseUpHandler);
 };
 
 const mouseMoveHandler = function (e) {
     const dx = e.clientX - x;
-    if ((ele.offsetLeft + dx) > right_limit || (ele.offsetLeft + dx) < left_limit) {
+    var ele_offset = ele.offsetLeft + dx
+
+    if ((ele_offset) > right_limit || (ele_offset) < left_limit) {
         return
     }
-    ele.style.left = `${ele.offsetLeft + dx}px`;
+    ele.style.left = `${ele_offset}px`;
+
+    inner.scrollLeft = (ele_offset / slide_bar_scroll_dist) * scroll_dist
 
     x = e.clientX;
 };
 
 const mouseUpHandler = function () {
     ele.classList.remove('active')
+    slider.classList.remove('active');
     document.removeEventListener('mousemove', mouseMoveHandler);
     document.removeEventListener('mouseup', mouseUpHandler);
 };
@@ -191,3 +230,6 @@ const mouseUpHandler = function () {
 ele.addEventListener('mousedown', mouseDownHandler);
 
 
+// Starting off with Frontend Displayed
+
+team(2)
