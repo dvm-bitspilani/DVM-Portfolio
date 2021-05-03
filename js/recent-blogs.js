f52d954f1bb726e84d9f9f57be8083451eefa08e
Priyanshu <priyanshushukla9801@gmail.com>
2021-05-03 22:36:53 +0530

    recent blogs date and author

--- js/recent-blogs.js
+++ js/recent-blogs.js
@@ -3,16 +3,22 @@ let main_arr = [
     name: "Blog2",
     image_link: "content-blogs/blog2/joshua-reddekopp-GkFQEOubrCo-unsplash.png",
     url: "",
+    author: "Name Surname",
+    date: "4th May 2021",
   },
   {
     name: "Blog2",
     image_link: "content-blogs/blog3/kevin-ku-w7ZyuGYNpRQ-unsplash.png",
     url: "",
+    author: "Name Surname",
+    date: "4th May 2021",
   },
   {
     name: "Blog2",
     image_link: "content-blogs/blog4/markus-spiske-hvSr_CVecVI-unsplash.png",
     url: "",
+    author: "Name Surname",
+    date: "4th May 2021",
   },
 ];
 
@@ -25,13 +31,18 @@ recent_blogs.innerHTML = `<div class="recent-blog-heading">
 let main_div = document.createElement("div");
 for (var i = 0; i < main_arr.length; i++) {
   main_div.innerHTML += `
-        <div class="recent-blogs-post">
-              <div class="other-post-image" onclick='location.href='${main_arr[i].url}'>
+        <div class="recent-blogs-post" onclick='location.href='${main_arr[i].url}'>
+              <div class="other-post-image" >
                 <img
                   src="${main_arr[i].image_link}"
                 />
               </div>
-              <div class="other-post-heading" onclick='location.href='${main_arr[i].url}'>${main_arr[i].name}</div>
+              <div class="other-post-heading" >${main_arr[i].name}</div>
+              <div class="other-post-details">
+                  <div class="other-post-author">${main_arr[i].author}</div>
+                  <div class="dot"></div>
+                  <div class="other-post-date">${main_arr[i].date}</div>
+              </div>
             </div>
     `;
 }
