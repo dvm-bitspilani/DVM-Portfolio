a144104eba8c59fcac1089403a45cc1e59cd253c
Nidheesh Jain <nidheeshjain01@gmail.com>
2021-05-07 17:09:29 +0530

    added loader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 var font = new FontFaceObserver("Jaapokki subtract");
+var loaderDestroying = false;
+let destroyerTriggerTime = null;
 font.load().then(() => {
   //This is only a demo for preloading fonts. Use ONLY for loader fonts strictly
   // setTimeout(() => {
@@ -16,8 +18,38 @@ console.log("scrolled");
 
 function allImagesLoaded() {
   console.log("ALL IMAGES LOADED");
+  let loaderContainer = document.querySelector(".loaderContainer");
+  let pentagon = document.querySelector(".pentagon");
 
-  document.getElementsByClassName("loader")[0].style.display = "none";
+  loaderContainer.addEventListener(
+    "animationend",
+    () => {
+      if (
+        destroyerTriggerTime != null &&
+        loaderDestroying
+      ) {
+        document.getElementsByClassName("loader")[0].style.display = "none";
+      }
+    },
+    false
+  );
+
+  loaderContainer.addEventListener(
+    "animationiteration",
+    function () {
+      currentTime = Math.round(Date.now() / 1000);
+      if (!loaderDestroying) {
+        destroyerTriggerTime = Math.round(Date.now() / 1000);
+        console.log(destroyerTriggerTime);
+        loaderDestroying = true;
+        loaderContainer.style.animation = "2s loader-disappear forwards";
+        pentagon.style.animation = "none";
+      }
+    },
+    false
+  );
+
+  // document.getElementsByClassName("loader")[0].style.display = "none";
   // for (let i = 0; i < pages.length; ++i) {
   //   let nameOfClass = `dot${i}`
   //   document.getElementsByClassName("section-nav")[0].innerHTML += `
